64066016c28eaddab6d405df23322b69698b6e2c
DANIIL GOLOTVIN <daniilgolotvin@yandex.ru>
2024-05-20 03:58:43 +0300

    100 try

--- src/components/map.js
+++ src/components/map.js
@@ -2,53 +2,47 @@ document.addEventListener('DOMContentLoaded', function() {
 	const mapButton = document.getElementById('map-button');
 
 	if (mapButton) {
-		mapButton.addEventListener('click', function() {
-			const mainContent = document.getElementById('main-content');
-			const mapContainer = document.getElementById('map-container');
-			const buttons = document.querySelectorAll('.header__bottom-button');
+			mapButton.addEventListener('click', function() {
+					const mainContent = document.getElementById('main-content');
+					const mapContainer = document.getElementById('map-container');
+					const buttons = document.querySelectorAll('.header__bottom-button');
 
-			if (mainContent && mapContainer) {
-				mainContent.style.display = 'none';
-				mapContainer.style.display = 'block';
+					if (mainContent && mapContainer) {
+							mainContent.style.display = 'none';
+							mapContainer.style.display = 'block';
 
-				if (typeof ymaps !== 'undefined') {
-					if (navigator.geolocation) {
-						navigator.geolocation.getCurrentPosition(function(position) {
-							const userCoordinates = [position.coords.latitude, position.coords.longitude];
-							init(userCoordinates);
-
-						}, function(error) {
-							console.error("Error getting user location:", error);
-							init([55.1910, 42.1023]);
-						});
-					} else {
-						console.error("Geolocation is not supported by this browser.");
-						init([55.1910, 42.1023]); 
+							if (typeof ymaps !== 'undefined') {
+									ymaps.ready(function() {
+											init([55.76, 37.64]); 
+									});
+							} else {
+									console.error("Yandex Maps library is not loaded.");
+							}
 					}
-				} else {
-					console.error("Yandex Maps library is not loaded.");
-				}
-			}
 
-			buttons.forEach(btn => {
-				if (btn.getAttribute('data-target') === 'map') {
-					btn.classList.add('active');
-				} else {
-					btn.classList.remove('active');
-				}
+					buttons.forEach(btn => {
+							if (btn.getAttribute('data-target') === 'map') {
+									btn.classList.add('active');
+							} else {
+									btn.classList.remove('active');
+							}
+					});
 			});
-		});
 	}
 });
 
 function init(coordinates) {
 	var myMap = new ymaps.Map("map-box", {
-		center: coordinates,
-		zoom: 10
+			center: coordinates,
+			zoom: 10
 	});
 
+	var myPlacemark = new ymaps.Placemark(coordinates, {
+			hintContent: 'Место проживания',
+			balloonContent: 'Здесь вы проживаете'
+	});
 
-
+	myMap.geoObjects.add(myPlacemark);
 
 	document.getElementById('preloader').style.display = 'none';
 }
